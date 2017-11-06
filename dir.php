74dde23da0b116f470532d31d2c70813d6d66444
li.zhuang <lizhuang12>
2017-11-06 19:01:17 +0800

    git-wiki

--- dir.php
+++ dir.php
@@ -17,8 +17,7 @@ function ListDir ($dirname)
     while(false !== ($entry= $Ld->read())) {
         $checkdir=$dirname."/".$entry;
         if(is_dir($checkdir)&&!preg_match("[^\.]",$entry)){
-            echo"<li><p>".$checkdir."当前<span style='color:#ff00a  
-    a'>是</span>目录</p></li>";
+            echo"<li><p>".$checkdir."当前<span style='color:#ff00a'>是</span>目录</p></li>";
             ListDir($checkdir);
         }else{
             echo"<li><p>".$entry."当前不是目录</p></li>";
