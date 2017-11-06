3e92ceb626ec42a62a74d817f382a58301386664
li.zhuang <lizhuang12>
2017-11-06 15:19:46 +0800

    git-wiki

--- index.php
+++ index.php
@@ -5,3 +5,10 @@
  * Date: 2017/11/6
  * Time: 下午2:51
  */
+
+date_default_timezone_set('Asia/Shanghai');
+require 'vendor/autoload.php';
+
+$git = new PHPGit\Git();
+$c = $git->clone('https://github.com/opqnext/opq-wiki.git', '/path/to/repo');
+var_dump($c);
