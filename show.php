523bdba7e103a6dbec5e44e2a7034b5a7ccc302f
li.zhuang <lizhuang12>
2017-11-08 17:46:01 +0800

    git-wiki

--- show.php
+++ show.php
@@ -5,7 +5,8 @@
  * Date: 2017/11/8
  * Time: 下午1:56
  */
-
+ini_set("display_errors", "On");
+error_reporting(E_ALL | E_STRICT);
 date_default_timezone_set('Asia/Shanghai');
 require 'vendor/autoload.php';
 
@@ -13,10 +14,9 @@ $git = new PHPGit\Git();
 
 $git->setRepository('/data/wiki.opqnext.com/wiki');
 
-$hash = strval($_GET['hash']);
-
-echo $hash;
+//$hash = strval($_GET['hash']);
+//
+//echo $hash;
 //var_dump($git->tree('master'));
 
-$c = $git->show('2ee3e2f22e5d8463944689662c3447c1ce0d1f32');
-var_dump($c);
+$c = $git->show('ea923f390f6dbf0caf041239379b62e2f4e4518c');
