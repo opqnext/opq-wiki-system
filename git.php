fee4f4915a876fddc43be7b09fb9ea3f222c8a8c
li.zhuang <lizhuang12>
2017-11-08 14:15:28 +0800

    git-wiki

---
+++ git.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: momo
+ * Date: 2017/11/8
+ * Time: 下午2:14
+ */
+echo '<pre>';
+system("git pull", $var);
+
+error_log('git pull '.date('Y-m-d H:i:s'));
