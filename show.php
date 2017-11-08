2fdbc1dd1c22171a5643b7096c8e3931cd1ef902
li.zhuang <lizhuang12>
2017-11-08 18:01:36 +0800

    git-wiki

--- show.php
+++ show.php
@@ -19,4 +19,5 @@ $git->setRepository('/data/wiki.opqnext.com/wiki');
 //echo $hash;
 //var_dump($git->tree('master'));
 
-$c = $git->show('ea923f390f6dbf0caf041239379b62e2f4e4518c',['format' => 'oneline']);
+
+$c = $git->show('ea923f390f6dbf0caf041239379b62e2f4e4518c',['format' => 'oneline','abbrev-commit']);
