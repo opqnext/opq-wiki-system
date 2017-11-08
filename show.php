2b635f41c75ded3c51bceb27c582401e8b8d749d
li.zhuang <lizhuang12>
2017-11-08 22:50:40 +0800

    git-wiki

--- show.php
+++ show.php
@@ -23,5 +23,6 @@ $a = ['oneline', 'short', 'medium', 'full', 'fuller', 'email'];
 
 $res =  $git->show($hash,['format'=>'oneline','abbrev-commit'=>true]);
 
-var_dump($res);
+echo "<pre>";
+echo $res;
 
