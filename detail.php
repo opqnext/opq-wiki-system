fd16b742fd14df34e41a7f76547319146477869d
li.zhuang <lizhuang12>
2017-11-08 13:32:20 +0800

    git-wiki

--- detail.php
+++ detail.php
@@ -9,6 +9,15 @@
 echo time();
 echo "<br>";
 
-echo './wiki/'.$_GET['id'].'.html';
+//echo './wiki/'.$_GET['id'].'.html';
+
+$git = new PHPGit\Git();
+
+$git->setRepository('/data/wiki.opqnext.com/wiki');
+
+$shortlog = $git->shortlog->summary();
+
+echo "<pre>";
+print_r($shortlog);
 
 include './wiki/'.$_GET['id'].'.html';
