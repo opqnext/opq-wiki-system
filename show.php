dac0b84614c8f44342bcfbede17a4029c8bcf46a
li.zhuang <lizhuang12>
2017-11-09 10:37:17 +0800

    git-wiki

--- show.php
+++ show.php
@@ -23,40 +23,19 @@ $a = ['oneline', 'short', 'medium', 'full', 'fuller', 'email'];
 
 $res =  $git->show($hash,['format'=>'oneline','abbrev-commit'=>true]);
 
+
 echo "<pre>";
 echo $res;
-htmlDiff($res,$res);
+?>
 
-function diff($old, $new){
-    $matrix = array();
-    $maxlen = 0;
-    foreach($old as $oindex => $ovalue){
-        $nkeys = array_keys($new, $ovalue);
-        foreach($nkeys as $nindex){
-            $matrix[$oindex][$nindex] = isset($matrix[$oindex - 1][$nindex - 1]) ?
-                $matrix[$oindex - 1][$nindex - 1] + 1 : 1;
-            if($matrix[$oindex][$nindex] > $maxlen){
-                $maxlen = $matrix[$oindex][$nindex];
-                $omax = $oindex + 1 - $maxlen;
-                $nmax = $nindex + 1 - $maxlen;
-            }
-        }
-    }
-    if($maxlen == 0) return array(array('d'=>$old, 'i'=>$new));
-    return array_merge(
-        diff(array_slice($old, 0, $omax), array_slice($new, 0, $nmax)),
-        array_slice($new, $nmax, $maxlen),
-        diff(array_slice($old, $omax + $maxlen), array_slice($new, $nmax + $maxlen)));
-}
-function htmlDiff($old, $new){
-    $ret = '';
-    $diff = diff(preg_split("/[\s]+/", $old), preg_split("/[\s]+/", $new));
-    foreach($diff as $k){
-        if(is_array($k))
-            $ret .= (!empty($k['d'])?"<del>".implode(' ',$k['d'])."</del> ":'').
-                (!empty($k['i'])?"<ins>".implode(' ',$k['i'])."</ins> ":'');
-        else $ret .= $k . ' ';
-    }
-    return $ret;
-}
+<!-- CSS -->
+<link rel="stylesheet" type="text/css" href="dist/diff2html.css">
 
+<!-- Javascripts -->
+<script type="text/javascript" src="dist/diff2html.js"></script>
+<script type="text/javascript" src="dist/diff2html-ui.js"></script>
+
+<script>
+    var diff2htmlUi = new Diff2HtmlUI({diff: '<?php echo $res;?>'});
+    diff2htmlUi.draw('html-target-elem', {inputFormat: 'json', showFiles: true, matching: 'lines'});
+</script>
