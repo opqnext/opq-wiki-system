f91b0aea97eec1680d61f79efc7edb6e76fb792c
li.zhuang <lizhuang12>
2017-11-09 11:50:37 +0800

    git-wiki

--- show.php
+++ show.php
@@ -24,7 +24,7 @@ $a = ['oneline', 'short', 'medium', 'full', 'fuller', 'email'];
 $res =  $git->show($hash,['format'=>'oneline','abbrev-commit'=>true]);
 
 
-echo str_replace(" ","\n",$res);
+var_dump($res);
 ?>
 <html>
     <head>
