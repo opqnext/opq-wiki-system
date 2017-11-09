d4b8b8823864e8b0d2ca8a902b2c23291ca9279a
li.zhuang <lizhuang12>
2017-11-09 11:22:36 +0800

    git-wiki

--- show.php
+++ show.php
@@ -52,9 +52,10 @@ echo $res;
 
         <script>
             $(document).ready(function() {
-                let diff2html = require("diff2html").Diff2Html;
+                //let diff2html = require("diff2html").Diff2Html;
                 var diff2htmlUi = new Diff2HtmlUI({diff: "f9eb2d4 修改了MTZ文件 diff --git a/8.html b/8.html index 4d9495e..6bce554 100644 --- a/8.html +++ b/8.html @@ -3,8 +3,9 @@ 听少爷说他在说这些不是我的错 难道说这么多都是孩儿惹的祸 阿玛说阿玛说人生不可重来过 -废话还是最好尽量地少说为妙 +1. 年轻的确很美但骄傲是你不对 - 应该关爱社会你也得感动落泪 - 不管你是否想要简化些的思维 -- 年轻的确很美但骄傲是你不对 \ No newline at end of file +1. 年轻的确很美但骄傲是你不对 +2. 年轻的确很美但骄傲是你不对"});
                 diff2htmlUi.draw('html-target-elem', {inputFormat: 'diff', showFiles: true, matching: 'lines'});
+                diff2htmlUi.fileListCloseable('#line-by-line', false);
             });
         </script>
     </body>
