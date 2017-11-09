4dad62f227aad0104afef0f082c58c9ef85477b5
li.zhuang <lizhuang12>
2017-11-09 17:28:53 +0800

    wiki-opq

--- controller/IndexController.php
+++ controller/IndexController.php
@@ -7,11 +7,21 @@
  */
 namespace controller;
 use core\controller\Controller;
+use PHPGit\Git;
 
 class IndexController extends Controller{
 
     public function index(){
         $this->assign('msg',"赋值");
         $this->display('index/index.html');
+    }
+
+    public function clone_wiki()
+    {
+        $git = new Git();
+
+        var_dump($git);
+        $c = $git->clone('https://github.com/opqnext/opq-wiki.git', './wiki');
+        var_dump($c);
     }
 } 
