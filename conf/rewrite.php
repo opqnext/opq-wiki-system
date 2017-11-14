505c3bf3c35f4cb156404fdb888c9c233438b5ca
li.zhuang <lizhuang12>
2017-11-14 14:52:01 +0800

    wiki-opq

--- conf/rewrite.php
+++ conf/rewrite.php
@@ -2,10 +2,12 @@
 use \NoahBuscher\Macaw\Macaw;
 
 Macaw::get('/','controller\IndexController@index');
-Macaw::get('wiki/(:num)','controller\IndexController@wiki');
-Macaw::get('add/(:num)','controller\IndexController@add');
+Macaw::get('wiki/(:num)','controller\IndexController@wiki');    // wiki详情页
+Macaw::get('add/(:num)','controller\IndexController@add');      // wiki增加
 Macaw::get('commit','controller\IndexController@commit');
 Macaw::post('ajax_new_wiki','controller\IndexController@ajax_new_wiki');
+
+Macaw::get('pull','controller\IndexController@pull_wiki');
 
 
 
