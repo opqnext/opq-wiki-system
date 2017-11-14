bcf73980c28f2b35672b56551cfbd043dffbbfa9
li.zhuang <lizhuang12>
2017-11-14 14:47:26 +0800

    filter new files

---
+++ model/AdminModel.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: momo
+ * Date: 2017/11/13
+ * Time: 上午10:06
+ */
+
+namespace model;
+
+
+use core\model\BaseModel;
+
+class AdminModel extends BaseModel
+{
+    public function getAdmin()
+    {
+        echo 'getAdmin';
+    }
+}
